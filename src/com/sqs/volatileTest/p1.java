d24d670f3067efe7b48daffeeb2ea7f8f1d9bca1
XIANG995 <chaizx_neu@163.com>
2021-09-22 17:28:08 +0800

    2021-9-22

--- src/com/sqs/volatileTest/p1.java
+++ src/com/sqs/volatileTest/p1.java
@@ -1,10 +1,28 @@
 package com.sqs.volatileTest;
 
-public class p1 extends Thread {
-
-
-    @Override
-    public void run() {
-        super.run();
+public class p1  {
+    public static void main(String[] args) {
+        for (int i =0;i<100;i++){
+          new MyThread().start();
+        }
     }
+
+
+    static class MyThread extends Thread{
+          public  static int count;
+
+        public  synchronized static void addCount(){
+                for (int i =0;i<1000;i++){
+                    count++;
+                }
+            System.out.println(Thread.currentThread().getName()+" count = "+count);
+        }
+
+        @Override
+        public void run() {
+            addCount();
+        }
+    }
+
+
 }
